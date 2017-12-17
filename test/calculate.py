d56c73772e23475a960cbe3d70218116f9071851
Shih-Chen <r06922135@csie.ntu.edu.tw>
2017-12-17 17:17:56 +0800

    Processing sampling file and plot

---
+++ test/calculate.py
@@ -0,0 +1,21 @@
+file_name = ["pow_c.txt", "pow_sse.txt", "pow_cl.txt"]
+output_name = "output.txt"
+
+fout = open(output_name, "w")
+fout.write("pow ")
+
+for name in file_name:
+    count = 0
+    acc = 0.0
+    
+    with open(name, "r") as f:
+        for line in f:
+            _, _, time, _ = line.split()
+            acc += float(time)
+            count += 1
+
+    avg = acc / count
+    fout.write(str(avg) + " ")
+
+fout.write("\n")
+fout.close()
